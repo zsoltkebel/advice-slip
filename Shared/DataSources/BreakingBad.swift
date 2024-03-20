f4914af375200fe3a8932f951876f922532cf0e6
Jack <harps912@gmail.com>
2024-03-20 17:48:59 +0000

    Add breaking bad quote source

---
+++ Shared/DataSources/BreakingBad.swift
@@ -0,0 +1,36 @@
+//
+//  Waltuh.swift
+//  AdviceSlip
+//
+//  Created by Jack on 20/03/2024.
+//
+
+import Foundation
+
+struct BreakingBadResponse: APIResponse {
+    var quote: String
+    var author: String
+    
+    func getContent() -> String { return quote }
+    
+    func getAuthor() -> String? { return author }
+}
+
+class BreakingBadDataSource: APIDataSource {
+    func getID() -> Int {
+        return DataSource.BreakingBadQuotes.rawValue
+    }
+    
+    func getName() -> String {
+        return "Breaking Bad Quotes"
+    }
+    
+    func getURL() -> URL? {
+        return URL(string: "https://api.breakingbadquotes.xyz/v1/quotes")
+    }
+    
+    func decode(from data: Data) throws -> APIResponse {
+        let quotes = try JSONDecoder().decode([BreakingBadResponse].self, from: data)
+        return quotes.first!
+    }
+}
--- Shared/DataSources/DataSource.swift
+++ Shared/DataSources/DataSource.swift
@@ -9,7 +9,7 @@ import Foundation
 
 enum DataSource: Int, CaseIterable, Identifiable {
     // Order matters!
-    case random, adviceSlip, kanyeRest, quotable, zenQuotes
+    case random, adviceSlip, kanyeRest, quotable, zenQuotes, BreakingBadQuotes
 
     var id: Int { return self.rawValue }
     
@@ -23,7 +23,7 @@ enum DataSource: Int, CaseIterable, Identifiable {
     func getAPIDataSource() -> APIDataSource {
         switch self {
         case .random:
-            return [AdviceSlipDataSource(), KanyeDataSource(), QuotableDataSource(), ZenQuotesDataSource()].randomElement() as! APIDataSource
+            return [AdviceSlipDataSource(), KanyeDataSource(), QuotableDataSource(), ZenQuotesDataSource(),BreakingBadDataSource()].randomElement() as! APIDataSource
         case .adviceSlip:
             return AdviceSlipDataSource()
         case .kanyeRest:
@@ -32,6 +32,8 @@ enum DataSource: Int, CaseIterable, Identifiable {
             return QuotableDataSource()
         case .zenQuotes:
             return ZenQuotesDataSource()
+        case .BreakingBadQuotes:
+            return BreakingBadDataSource()
         }
     }
 }
